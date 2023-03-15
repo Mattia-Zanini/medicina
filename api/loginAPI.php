51910b02db9ee20575f0e9cce280ff5dd6287bcd
Mattia-Zanini <mattzanini2@gmail.com>
2023-03-15 12:09:09 +0100

    Update loginAPI.php

--- api/loginAPI.php
+++ api/loginAPI.php
@@ -5,7 +5,7 @@ session_start();
 print_r($baseURL);
 
 if (isset($_POST["mail"]) == true && isset($_POST["password"]) == true) {
-    if ($_POST["mail"] == "ad@gmail.com" && $_POST["password"] == "1234") {
+    if ($_POST["mail"] == "a@gmail.com" && $_POST["password"] == "1234") {
         $_SESSION['logged_in'] = true;
         Redirect($baseURL);
     }
